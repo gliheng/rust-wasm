60f1146b9cd4db1c5289f5bdd5d27e1bb87904f6
Amadeus <gliheng@gmail.com>
2017-12-19 17:58:42 +0800

    ffi string param

--- interop/src/main.rs
+++ interop/src/main.rs
@@ -1,5 +1,6 @@
-fn main() {
-}
+use std::mem;
+use std::os::raw::c_char;
+use std::ffi::CString;
 
 #[no_mangle]
 pub fn rust_pow(number: i32, power: i32) -> i32 {
@@ -9,4 +10,20 @@ pub fn rust_pow(number: i32, power: i32) -> i32 {
 #[no_mangle]
 pub fn hello() {
     println!("Hello, world!");
+}
+
+#[no_mangle]
+pub fn draw(sel: *mut c_char) {
+    unsafe {
+        match CString::from_raw(sel).into_string() {
+            Ok(sel) => {
+                println!("I'll draw on: {}", sel);
+                mem::forget(sel);
+            },
+            Err(_) => ()
+        }
+    }
+}
+
+fn main() {
 }
